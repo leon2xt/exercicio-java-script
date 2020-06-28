d272ee0ba72d4c0b62cd03693b0809dbac9139ea
Pablo Leon <67494369+leon2xt@users.noreply.github.com>
2020-06-28 12:59:24 -0300

    Alguns Exercícios Fundamentos

---
+++ comentario.js
@@ -0,0 +1,16 @@
+// Comentário de uma Linha
+console.log('Linha 1')
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console.log('linha 2')
+
+/*
+*Comentário de
+*multiplas
+*linhas
+*/
+console.log('linha 3')
+
